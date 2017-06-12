9b49d9b973e0023ab36b3b35f37868d3ee23a9a3
Gergely Rozsonits <v-grozsonits@expedia.com>
2017-06-12 17:03:03 +0200

    adding missing fragment of create keyspace and fixing clustering key

--- 01-create-script.sql
+++ 01-create-script.sql
@@ -3,7 +3,7 @@
  -- 03-query-readonly.sql
 
 -- Create keyspace (press TAB for autocomplete in cqlsh)
-CREATE KEYSPACE homework
+CREATE KEYSPACE homework WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };
 
 -- Use keyspace
 USE homework;
@@ -20,7 +20,7 @@ CREATE table invoice
   line_id int,
   article_name text,
   article_price decimal,
-  PRIMARY KEY (invoice_id, article_name)
+  PRIMARY KEY (invoice_id, article_name, line_id)
 );
 
 CREATE INDEX ON invoice(invoice_date);
